9d6ad3ff85af93f24cc04a8353dfae882d69f2da
Wonjong Park <wonjong2@gmail.com>
2022-03-15 21:03:34 -0700

    Updated files with 2nd implementation

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Week4_Code_Quiz
+
+## Description
+
+This Web site generates a password to suggest user. 
+When user clicks "Generate Password" button, it asks user about some questions such as 'how many characters in a password' and 'what types of characters should be included in it'.
+After user answers these questions, a new password which is generated based on user's answer will appear on the screen.
+
+- Use 
+- Store top5-highscore data in localStorage
+- 
+
+## Web Site's URL
+
+- Code Quiz : 
+https://wonjong2.github.io/Week4_Code_Quiz/
+
+## Git Hub URL for this Web Site
+- wonjong2/Week4_Code_Quiz : https://github.com/wonjong2/Week4_Code_Quiz
+
+## Usage
+
+- User clicks "Generate Passworkd" button to have a new password.
+- If user inputs the number not in the valid range(8~128), the web site asks user it again until user inputs valid number.
+- If user doesn't select any character types, the web site asks user them again until user selects at least one character type.
+- When all inputs from user are validated, the web site generates a new password and display it on the screen.
+- __Simple Demo__ : https://watch.screencastify.com/v/ImMADFavFRHkFsOV2UiU
+
+Please see the screenshots on this Website.
+
+- The first screen when user opens this web site.<br>
+    ![First Page](images/launchScreen.jpg)
+
+- User needs to answer serveral questions. Please see the validation flow of the user inputs.<br>
+    ![Questions](/images/validateflow.jpg)
+
+- A new password shown on the screen. <br>
+    ![Result](/images/result.jpg) 
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,69 +12,51 @@ var liEl = document.querySelectorAll("li");
 var submitButton = document.querySelector("#submit");
 var backButton = document.querySelector("#back");
 var clearButton = document.querySelector("#clear");
+var highscoreTitle = document.querySelector("#highscore-title");
+var highscoreList = document.querySelector("#top10list");
 
-// time limitation 75 secs
-var timeInitialValue = 15;
+var timeInitialValue = 10*quizData.length; // Initial value of the timer
 var timeRemain; 
 var timeInterval;
-// var resultInterval;
-
-// User Score variable
+var resultInterval;
 var userScore;
-
-// Highscore lists
-var top5Scorer = [];
-
+var top10Scorer = [];
 var quizIndex;
+var comeFrom;
+
+viewHighScore.addEventListener("mouseover", cursorChange);
+viewHighScore.addEventListener("click", highScorePage);
+startButton.addEventListener("mouseover", cursorChange);
+startButton.addEventListener("click", startQuiz);
+submitButton.addEventListener("mouseover", cursorChange);
+submitButton.addEventListener("click", checkInitials);
+backButton.addEventListener("mouseover", cursorChange);
+backButton.addEventListener("click", decideToWhere);
+clearButton.addEventListener("mouseover", cursorChange);
+clearButton.addEventListener("click", clearHighscores);
+
+for(var k = 0; k < 4; k++) {
+    liEl[k].addEventListener("mouseover", multiChoiceBgColorChange);
+    liEl[k].addEventListener("mouseout", multiChoiceBgColorChange);
+    liEl[k].addEventListener("click", checkAnswer);
+}
 
 // mouseover listener of the button with id="start"
 function cursorChange(event) {
     event.target.style.cursor = "pointer";
 }
 
-// function startBtnCursorChange() {
-//     startButton.setAttribute("style", "font-size: 1.2rem; color: white; background-color: purple; text-align: center; border: 1px purple solid; border-radius: 5px; padding: 2px 10px; cursor: pointer;");
-//     return;
-// }
-
-// mouseover listener of the button with id="submit"
-// function submitBtnCursorChange() {
-//     submitButton.setAttribute("style", "display: inline-block; font-size: 1.2rem; color: white; background-color: purple; text-align: center; border: 1px purple solid; border-radius: 5px; padding: 2px 10px; cursor: pointer;");
-//     return;
-// }
-
-// // mouseover listener of the button with id="back"
-// function backBtnCursorChange() {
-//     backButton.setAttribute("style", "display: inline-block; font-size: 1.2rem; color: white; background-color: purple; text-align: center; border: 1px purple solid; border-radius: 5px; padding: 2px 10px; margin-right: 10px; cursor: pointer;");
-//     return;
-// }
-
-// // mouseover listener of the button with id="clear"
-// function clearBtnCursorChange() {
-//     clearButton.setAttribute("style", "display: inline-block; font-size: 1.2rem; color: white; background-color: purple; text-align: center; border: 1px purple solid; border-radius: 5px; padding: 2px 10px; cursor: pointer;");
-//     return;
-// }
-
-// // 
-// function viewHsCursorChange() {
-//     // viewHighScore.setAttribute("style", "color: purple; padding: 10px; font-size: 1.5rem; cursor: pointer;");
-//     viewHighScore.style.cursor = "pointer";
-// }
-
 function startQuiz() {   
-    // Remove eventlistener from startButton
-    // startButton.removeEventListener("mouseover", startBtnCursorChange);
-    startButton.removeEventListener("mouseover", cursorChange);
-    startButton.removeEventListener("click", startQuiz);
-
+    comeFrom = "Quiz page";
     // Starts timer, intial value is 75 secs
     timeRemain = timeInitialValue;
     time.textContent = "Time: " + timeRemain;
 
     // Clear contents of the main page
-    title.setAttribute("style", "display: none");
-    description.setAttribute("style", "display: none");
-    startButton.setAttribute("style", "display: none");
+    setDisplayProperty(".quiz-container", "flex");
+    setDisplayProperty(".firstpage-container", "none");
+    setDisplayProperty(".result-container", "none");
+    setDisplayProperty(".highscore-container", "none");
 
     // initialize user score
     userScore = 0;
@@ -88,34 +70,38 @@ function startQuiz() {
 function multiChoiceBgColorChange(event) {
     var element = event.target;
 
+    // change the background color of li element when mouse event occurs
     if(event.type == "mouseover") {
-        element.setAttribute("style", "display: block; background-color: purple;");
+        element.style.background = "rgb(209, 160, 209)";
+        element.style.border = "1px rgb(209, 160, 209) solid"
     }
     else {
-        element.setAttribute("style", "display: block; background-color: white;");
+        element.style.background = "purple";
+        element.style.border = "1px purple solid"
     }
 }
 
 function showQuiz(index) {
     // put quiz data into elements
     question.textContent = quizData[index][0];
+    question.style.marginBottom = "1rem";
 
     for(var i = 0; i < 4; i++) {
-        liEl[i].textContent = quizData[index][i+1];
-        liEl[i].setAttribute("style", "display: block;");
-    }
-
-    // add eventlistener to ulEl for mouseover, mouseout and click
-    if(index === 0){
-        ulEl.addEventListener("mouseover", multiChoiceBgColorChange);
-        ulEl.addEventListener("mouseout", multiChoiceBgColorChange);
-        ulEl.addEventListener("click", checkAnswer);
+        if(quizData[index][i+1] !== "") {
+            liEl[i].textContent = quizData[index][i+1];
+            liEl[i].style.backround = "purple";
+            liEl[i].style.border = "1px purple solid"
+            liEl[i].style.display = "block";
+        }
+        else {
+            liEl[i].style.display = "none";
+        }
     }
 
     timeInterval = setInterval(() => {
         if(timeRemain <= 0) {
             clearInterval(timeInterval);
-            gameResult();
+            finalResult();
             return;
         }
         else{
@@ -129,60 +115,44 @@ function checkAnswer(event) {
     var element = event.target;
     // if user answer is correct, show "Correct!!" message 
     // show "Wrong!!" message if it's incorrect
-    if(element.id == quizData[quizIndex][5]) {
-        console.log("Correct!!");
+    if(element.dataset.id == quizData[quizIndex][5]) {
         document.querySelector("#result").textContent = "Correct!";
+        document.querySelector("#result").style.color = "blue";
         userScore++;
     }
     else {
-        console.log("Wrong!!");
         document.querySelector("#result").textContent = "Wrong!";
+        document.querySelector("#result").style.color = "red";
         timeRemain -=10;
     }
 
-    var resultInterval = setTimeout(() => {
+    resultInterval = setTimeout(() => {
         clearInterval(timeInterval);
         document.querySelector("#result").textContent = "";
         // if user went through all quesions, go to game result screen
         // if there are any questions to be answered, show the next question
         if(quizData.length === quizIndex+1) {
-            gameResult();
+            finalResult();
             return;
         }
         else {
             quizIndex++;
             showQuiz(quizIndex); // display the next question
         }
-    }, 500);
+    }, 700);
 }
 
-function gameResult() {
-    // remove eventlistener from ulEl for mouseover, mouseout and click
-    ulEl.removeEventListener("mouseover", multiChoiceBgColorChange);
-    ulEl.removeEventListener("mouseout", multiChoiceBgColorChange);
-    ulEl.removeEventListener("click", checkAnswer);
-
-    question.textContent = "";
-
-    for(var i = 0; i < liEl.length; i++) {
-        liEl[i].setAttribute("style", "display: none;")
-    }
-
+function finalResult() {
+    comeFrom = "Result page";
     time.textContent = "Time: 0";
-
-    title.textContent = "All done!";
-    title.setAttribute("style", "font-size: 3rem; font-weight: bold; color: black; margin-bottom: 1rem");
-
-    description.textContent = "Your final score is " + userScore; 
-    description.setAttribute("style", "width: 60%; font-size: 1.2rem; color: black; text-align: center; text-align-last: center; margin-bottom: 1rem");
-
-    document.querySelector("label").setAttribute("style", "display: inline-block");
-    document.querySelector("#initial").setAttribute("style", "display: inline-block");
+    document.querySelector("#score").textContent = "Your final score is " + userScore;
     document.querySelector("#initial").value = "";
-    submitButton.setAttribute("style", "display: inline-block; font-size: 1.2rem; color: white; background-color: purple; text-align: center; border: 1px purple solid; border-radius: 5px; padding: 2px 10px;");
 
-    submitButton.addEventListener("mouseover", cursorChange);
-    submitButton.addEventListener("click", checkInitials);
+    setDisplayProperty(".header-container", "flex");
+    setDisplayProperty(".quiz-container", "none");
+    setDisplayProperty(".firstpage-container", "none");
+    setDisplayProperty(".result-container", "flex");
+    setDisplayProperty(".highscore-container", "none");
 }
 
 function checkInitials() {
@@ -194,121 +164,106 @@ function checkInitials() {
         return;
     }
 
-    // remove eventlistener from submitButton
-    submitButton.removeEventListener("mouseover", cursorChange);
-    submitButton.removeEventListener("click", checkInitials);
-
     makeHsList(enteredInitial.value, userScore);
+    comeFrom = "First page";
     highScorePage();
 }
 
 // Add user score and initials to the Top5 list and then sort it
 function makeHsList(initial, score) {
-    top5Scorer.push([score, initial]);
-    top5Scorer.sort(function(a,b){return b[0]-a[0]});
-    while(top5Scorer.length > 5){
-        top5Scorer.pop();
+    top10Scorer.push([score, initial]);
+    top10Scorer.sort(function(a,b){return b[0]-a[0]});
+    while(top10Scorer.length > 10){
+        top10Scorer.pop();
     }
+    localStorage.setItem("top10", JSON.stringify(top10Scorer));
     return;
 }
 
 function highScorePage() {
-    var headerEl = document.querySelectorAll("h1");
-    headerEl[0].setAttribute("style", "display: none");
-    headerEl[1].setAttribute("style", "display: none");
-    // headerEl[0].textContent = "";
-    // headerEl[1].textContent = "";
+    if(comeFrom === "Quiz page") {
+        clearInterval(timeInterval);
+        clearInterval(resultInterval);        
+    }
+    // get the number of li elements that already exist
+    var curListLength = highscoreList.children.length;
+    var maxNum = (top10Scorer.length<10)?top10Scorer.length:10;
 
-    description.setAttribute("style", "display: none;");
-    document.querySelector("label").setAttribute("style", "display: none;");
-    document.querySelector("#initial").setAttribute("style", "display: none;");
-    document.querySelector("#submit").setAttribute("style", "display: none;");
+    setDisplayProperty(".header-container", "none");
+    setDisplayProperty(".quiz-container", "none");
+    setDisplayProperty(".firstpage-container", "none");
+    setDisplayProperty(".result-container", "none");
+    setDisplayProperty(".highscore-container", "flex");
 
-    title.textContent = "Highscores";
-    for(var i = 0; i < liEl.length; i++) {
-        if(i < top5Scorer.length){
-            liEl[i].textContent = i+1 + ". " + top5Scorer[i][1] + " - " + top5Scorer[i][0];
-            liEl[i].setAttribute("style", "display: block;");
+    for(var i = 0; i < maxNum; i++) {
+        if((i + 1) <= curListLength) {
+            highscoreList.children[i].textContent = i+1 + ". " + top10Scorer[i][1] + " - " + top10Scorer[i][0];
         }
         else {
-            liEl[i].textContent = "";
-            liEl[i].style.display = "none";
+            var newListItem = document.createElement("li");
+            newListItem.textContent = i+1 + ". " + top10Scorer[i][1] + " - " + top10Scorer[i][0];
+            highscoreList.appendChild(newListItem);
         }
     }
 
-    backButton.setAttribute("style", "display: inline-block; font-size: 1.2rem; color: white; background-color: purple; text-align: center; border: 1px purple solid; border-radius: 5px; padding: 2px 10px; margin-right: 10px");
-    backButton.addEventListener("mouseover", cursorChange);
-    backButton.addEventListener("click", remEvtListenerNGoMain);
-
-    clearButton.setAttribute("style", "display: inline-block; font-size: 1.2rem; color: white; background-color: purple; text-align: center; border: 1px purple solid; border-radius: 5px; padding: 2px 10px;");
-    clearButton.addEventListener("mouseover", cursorChange);
-    clearButton.addEventListener("click", clearHighscores);
 }
 
-function remEvtListenerNGoMain() {
-    backButton.removeEventListener("mouseover", cursorChange);
-    backButton.removeEventListener("click", remEvtListenerNGoMain);
-    clearButton.removeEventListener("mouseover", cursorChange);
-    clearButton.removeEventListener("click", clearHighscores);
-
-    mainPage();
+function decideToWhere() {
+    switch(comeFrom){
+        case "Quiz page":
+            returnToQuiz();
+            break;
+        case "Result page":
+            finalResult();
+            break;
+        default:
+            mainPage();
+            break;
+    }
 }
 
-function remEvtListenerNHighscores() {
-    startButton.style.display = "none";
-    viewHighScore.removeEventListener("mouseover", cursorChange);
-    viewHighScore.removeEventListener("click", remEvtListenerNHighscores);
-    // startButton.removeEventListener("mouseover", startBtnCursorChange);
-    startButton.removeEventListener("mouseover", cursorChange);
-    startButton.removeEventListener("click", startQuiz);
-
-    highScorePage();
+function returnToQuiz() {
+    // Clear contents of the main page
+    setDisplayProperty(".header-container", "flex");
+    setDisplayProperty(".quiz-container", "flex");
+    setDisplayProperty(".firstpage-container", "none");
+    setDisplayProperty(".result-container", "none");
+    setDisplayProperty(".highscore-container", "none");
+    
+    // function call to display quiz
+    showQuiz(quizIndex);
 }
 
 function clearHighscores() {
-    for(var i = 0; i < top5Scorer.length ; i++) {
-        top5Scorer.pop();
+    // var curListLength = highscoreList.children.length;
+    while(highscoreList.firstElementChild) {
+        highscoreList.removeChild(highscoreList.firstElementChild);
     }
+    top10Scorer = [];
+    localStorage.removeItem("top10");
     highScorePage();
 }
 
 function mainPage() {
-    // set the h1 element for 'View Highscore'
-    viewHighScore.setAttribute("style", "color: purple; padding: 10px; font-size: 1.5rem");
-    // Add eventlisteners for 'View Highscre' 
-    viewHighScore.addEventListener("mouseover", function(){viewHighScore.setAttribute("style", "color: purple; padding: 10px; font-size: 1.5rem; cursor: pointer;")});
-    viewHighScore.addEventListener("click", remEvtListenerNHighscores);
-
-    // set the h1 element for 'Time:'
-    time.textContent = "Time: 0";
-    time.setAttribute("style", "color: black; padding: 10px; font-size: 1.5rem");
-
-    // Set the element h2 for the title
-    title.textContent = "Coding Quiz Challenge";
-    title.setAttribute("style", "font-size: 3rem; font-weight: bold; color: black; margin-bottom: 1rem");
-
-    // Set the element p for the description
-    description.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by tem seconds!";
-    description.setAttribute("style", "width: 60%; font-size: 1.2rem; color: black; text-align: center; text-align-last: center; margin-bottom: 1rem");
-
-    // Set the button for 'Start Quiz'
-    startButton.textContent = "Start Quiz";
-    startButton.setAttribute("style", "font-size: 1.2rem; color: white; background-color: purple; text-align: center; border: 1px purple solid; border-radius: 5px; padding: 2px 10px;");
-    // Add eventlisteners for 'Start Quiz' 
-    // startButton.addEventListener("mouseover", startBtnCursorChange);
-    startButton.addEventListener("mouseover", cursorChange);
-    startButton.addEventListener("click", startQuiz);
-
-    for(var i = 0; i < liEl.length; i++) {
-        liEl[i].setAttribute("style", "display: none;")
-    }
-
-    document.querySelector("label").setAttribute("style", "display: none");
-    document.querySelector("#initial").setAttribute("style", "display: none");
-    submitButton.setAttribute("style", "display: none");
-
-    backButton.setAttribute("style", "display: none");
-    clearButton.setAttribute("style", "display: none");
+    setDisplayProperty(".header-container", "flex");
+    setDisplayProperty(".firstpage-container", "flex");
+    setDisplayProperty(".quiz-container", "none");
+    setDisplayProperty(".result-container", "none");
+    setDisplayProperty(".highscore-container", "none");
+    comeFrom = "First page";
 }
 
-mainPage();
+function setDisplayProperty(selector, value) {
+    document.querySelector(selector).style.display = value;
+    return;
+}
+
+function init() {
+    top10Scorer = JSON.parse(localStorage.getItem("top10"));
+    if(!top10Scorer) {
+        top10Scorer = [];
+    }
+    mainPage();
+}
+
+init();
